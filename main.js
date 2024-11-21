7e3a2dd4a379c3af407eb55b58806133b6aefab7
gittan90 <anna.dohmers@gmail.com>
2024-11-21 14:06:57 +0100

    Implemented a remove button + styling

--- main.js
+++ main.js
@@ -7,16 +7,30 @@ document.addEventListener("DOMContentLoaded", function() {
     const addTodoButton = document.getElementById("add-todo-button");
 
     let todoArray = [
-        {task: "Yoga", completed: false},
-        {task: "Movement", completed: false},
-        {task: "Study", completed: false},
+        {id: 0, task: "Yoga", completed: false},
+        {id: 1, task: "Movement", completed: false},
     ];
 
     // Create todo list
     const todoListElement = document.createElement("ol");
 
     const createTodoElement = (task) => {
+        // Get the highest todo id
+        const highestId = Math.max(...todoArray.map(todo => todo.id));
+
+        // Create the new todo object
+        const newTodo = {
+            id: highestId + 1,
+            task,
+            completed: false,
+        };
+        // Push to the todoArray
+        todoArray.push(newTodo);
+        // Create task element
         const newTaskElement = document.createElement("li");
+        const todoId = `todo-${newTodo.id}`;
+        newTaskElement.id = todoId;
+
         // Create the checkbox element
         const newCheckboxElement = document.createElement("input");
         newCheckboxElement.type = "checkbox"
@@ -25,6 +39,29 @@ document.addEventListener("DOMContentLoaded", function() {
         const newTaskTextElement = document.createElement("p");
         newTaskTextElement.innerText = task;
         newTaskElement.appendChild(newTaskTextElement);
+
+        // Create the remove button
+        const newCheckboxRemoveButton = document.createElement("button");
+        newCheckboxRemoveButton.innerText = "X";
+        newCheckboxRemoveButton.className = "remove-button";
+        const newId = `todo-remove-button-${newTodo.id}`;
+        newCheckboxRemoveButton.id = newId;
+        // Listen to remove button
+        newCheckboxRemoveButton.addEventListener("click", () => {
+            // Remove the todo by creating a new array 
+            const newTodoArray = todoArray.map(i => {
+                if (i.id !== newId) {
+                    return i;
+                } 
+            });
+            todoArray = newTodoArray;
+            // Remove from DOM
+            const targetedElement = document.getElementById(todoId);
+            targetedElement.remove();
+        });
+
+        newTaskElement.appendChild(newCheckboxRemoveButton);
+
         // Append the full task element
         todoListElement.appendChild(newTaskElement);
     };
@@ -41,8 +78,8 @@ document.addEventListener("DOMContentLoaded", function() {
             task: todoInputElement.value, 
             completed: false
         };
-        todoArray.push(newTodo);
         // Append to the DOM
         createTodoElement(newTodo.task);
+        todoInputElement.value = "";
     });
 })
