57b4141c7df6b9b36c3409d2bcbeff0cbb53b5d6
gittan90 <anna.dohmers@gmail.com>
2024-11-21 13:19:47 +0100

    Added an input and add button + styling

--- main.js
+++ main.js
@@ -3,8 +3,10 @@ import "./style.css";
 document.addEventListener("DOMContentLoaded", function() {
 
     const app = document.getElementById("app");
+    const todoInputElement = document.getElementById("todo-input");
+    const addTodoButton = document.getElementById("add-todo-button");
 
-    const todos = [
+    let todoArray = [
         {task: "Yoga", completed: false},
         {task: "Movement", completed: false},
         {task: "Study", completed: false},
@@ -13,19 +15,34 @@ document.addEventListener("DOMContentLoaded", function() {
     // Create todo list
     const todoListElement = document.createElement("ol");
 
-    todos.forEach(i => {
+    const createTodoElement = (task) => {
         const newTaskElement = document.createElement("li");
         // Create the checkbox element
         const newCheckboxElement = document.createElement("input");
         newCheckboxElement.type = "checkbox"
         newTaskElement.appendChild(newCheckboxElement);
-        // Create the task element
+        // Create the text element
         const newTaskTextElement = document.createElement("p");
-        newTaskTextElement.innerText = i.task;
+        newTaskTextElement.innerText = task;
         newTaskElement.appendChild(newTaskTextElement);
         // Append the full task element
         todoListElement.appendChild(newTaskElement);
-    });
+    };
 
+    // Create and append all todo elements
+    todoArray.forEach(i => {
+        createTodoElement(i.task);
+    });
     app.appendChild(todoListElement);
+
+    // Add todo items to the todoArray on click 
+    addTodoButton.addEventListener("click", () => {
+        const newTodo = {
+            task: todoInputElement.value, 
+            completed: false
+        };
+        todoArray.push(newTodo);
+        // Append to the DOM
+        createTodoElement(newTodo.task);
+    });
 })
